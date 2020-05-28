b2356198d1dd40a15d3d655c6a138826c48e2c6a
sabovyan <abovyan.sargis90@gmail.com>
2020-05-28 14:27:12 +0400

    Remove duplicated directory

--- home-work-3/classwork/find.js
+++
@@ -1,9 +0,0 @@
-function find(obj, item) {
-  // eslint-disable-next-line no-restricted-syntax
-  for (const key of Object.keys(obj)) {
-    if (key === item) {
-      return item;
-    }
-  }
-  return 'Oops! Your bad!';
-}
--- home-work-3/classwork/subStrings.js
+++
@@ -1,9 +0,0 @@
-function subSets(arr) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i += 3) {
-    newArr.push(arr.slice(i, i + 3));
-  }
-  return newArr;
-}
-
-console.log(subSets([1, 2, 3, 4, 5, 6, 7])); // [1,2,3], [4,5,6], [7]
