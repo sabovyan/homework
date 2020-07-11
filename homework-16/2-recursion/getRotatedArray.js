182478eb5d7297045aadc9bbff77806b4911d2b6
sabovyan <abovyan.sargis90@gmail.com>
2020-07-11 13:09:05 +0400

    last recursion exercise

--- homework-16/2-recursion/getRotatedArray.js
+++ homework-16/2-recursion/getRotatedArray.js
@@ -1,12 +1,16 @@
 function getRotatedArray(arr, num) {
-	let res = [];
-	/* if (num === 0) {
-		return res;
-	} */
-	res = arr.splice(0, num);
-	console.log(arr);
-	return res;
+	function rotateArray(arr, num, res, count = 0) {
+		if (count === 1) {
+			return res;
+		}
+		res.unshift(arr.slice(num));
+		count++;
+		res.concat(rotateArray(arr.slice(num), num, res, count));
+		return res.flat();
+	}
+
+	return rotateArray(arr, num, arr.slice(0, num));
 }
 
-console.log(getRotatedArray(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], 3));
-// console.log(getRotatedArray(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], -2));
+console.log(getRotatedArray(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], 7));
+console.log(getRotatedArray(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], -2));
