d5fa6f2af2a8f3f1242237de033c3fff3f87c972
sabovyan <abovyan.sargis90@gmail.com>
2020-08-01 23:17:58 +0400

    added homework-18 first exercise

---
+++ homework-18/1-ghibli/index.js
@@ -0,0 +1,55 @@
+const body = document.querySelector('.movie__body');
+
+/**
+ *
+ * @param  {...any} items
+ * @returns {HTMLCollection} collections of td
+ * @example
+ *  createHTML(title, release_date, director, description);
+ */
+function createHTML(...items) {
+  const tr = document.createElement('tr');
+  for (let i = 0; i < items.length; i += 1) {
+    const td = document.createElement('td');
+    td.classList.add('movie__item');
+    td.textContent = items[i];
+    tr.append(td);
+    body.append(tr);
+  }
+  return tr;
+}
+
+/**
+ * @type {object} options for animations
+ */
+const appearOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px -250px 0px',
+};
+
+// eslint-disable-next-line no-shadow
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    entry.target.classList.add('appear');
+    appearOnScroll.unobserve(entry.target);
+  });
+}, appearOptions);
+
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then((r) => r.json())
+  .then((json) => {
+    json.sort((a, b) => (a.release_date > b.release_date ? -1 : 1));
+    // eslint-disable-next-line camelcase
+    json.forEach(({ title, release_date, director, description }) => {
+      createHTML(title, release_date, director, description);
+    });
+  })
+  .then(() => {
+    const trs = document.querySelectorAll('tr');
+    trs.forEach((tr) => {
+      appearOnScroll.observe(tr);
+    });
+  });
