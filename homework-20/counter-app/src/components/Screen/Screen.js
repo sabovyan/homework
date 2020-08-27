76bb4af06b98051903ffd10eb4f985187515d8eb
sabovyan <abovyan.sargis90@gmail.com>
2020-08-27 23:47:15 +0400

    styled screen component

--- homework-20/counter-app/src/components/Screen/Screen.js
+++ homework-20/counter-app/src/components/Screen/Screen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import './Screen.css';
 export default class Screen extends Component {
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
-		return <div>{this.props.screenText}</div>;
+		return <div className="screen">{this.props.screenText}</div>;
 	}
 }
