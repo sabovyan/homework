f9b90779cd0184da553da98ad0bfe7dd80f6516d
sabovyan <abovyan.sargis90@gmail.com>
2020-08-28 12:54:03 +0400

    added components to app.js, added event handlers

--- homework-20/counter-app/src/App.js
+++ homework-20/counter-app/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Button from './components/Button/Button';
 import Screen from './components/Screen/Screen';
-
-// const buttons = ['Increase', 'Decrease', 'Reset'];
+import Input from './components/Input/Input';
+import Range from './components/Range/Range';
+import './App.css';
 
 class App extends Component {
 	constructor(props) {
@@ -10,6 +11,8 @@ class App extends Component {
 		this.state = {
 			screenText: 0,
 			step: 1,
+			minValue: 0,
+			maxValue: 10,
 		};
 	}
 	handleIncrease = () => {
@@ -26,22 +29,68 @@ class App extends Component {
 
 	handleReset = () => {
 		this.setState((prevState) => ({
-			screenText: 0,
+			screenText: prevState.minValue || 0,
 		}));
 	};
 
+	handleMinValue = ({ target: { value } }) => {
+		// const normalizedValue = Number(e.target.value);
+		if (value < 0) {
+			value = 0;
+		}
+		this.setState({
+			minValue: Number(value),
+			screenText: Number(value),
+		});
+	};
+
+	handleMaxValue = ({ target: { value } }) => {
+		this.setState({
+			maxValue: Number(value),
+		});
+	};
+
+	handleStep = ({ target: { value } }) => {
+		this.setState({
+			step: Number(value),
+		});
+	};
+
 	render() {
-		const { screenText, btnDisabled } = this.state;
+		const { screenText, minValue, maxValue } = this.state;
+
 		return (
 			<div className="App">
 				<Screen screenText={screenText} />
-				<Button text="⬆" onClick={this.handleIncrease} />
 				<Button
-					disabled={screenText <= 0 ? true : false}
+					disabled={screenText >= maxValue ? true : false}
+					text="⬆"
+					onClick={this.handleIncrease}
+				/>
+				<Button
+					disabled={screenText <= minValue ? true : false}
 					text="⬇"
 					onClick={this.handleDecrease}
 				/>
-				<Button text="🔄" onClick={this.handleReset} />
+				<Button text="&#10226;" onClick={this.handleReset} />
+				<div className="inputs">
+					<Input
+						labelName="min"
+						value={minValue}
+						onChange={this.handleMinValue}
+					/>
+					<Input
+						labelName="max"
+						value={maxValue}
+						onChange={this.handleMaxValue}
+					/>
+					<Range
+						min={1}
+						max={this.state.maxValue}
+						step={this.state.step}
+						onChange={this.handleStep}
+					/>
+				</div>
 			</div>
 		);
 	}
