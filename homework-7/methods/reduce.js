e930fdbe4dd1b9600689371b5dd62ca0e895f046
sabovyan <abovyan.sargis90@gmail.com>
2020-09-11 13:43:39 +0400

    fixed case with initial value

--- homework-7/methods/reduce.js
+++ homework-7/methods/reduce.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 function reduceMethod(arr, func, initialValue) {
-	let initial = initialValue;
-	if (initialValue === null || initialValue === undefined) {
-		initial = 0;
-	}
-	let sum = initial;
-	for (let i = 0; i < arr.length; i += 1) {
-		sum = func.call(arr, sum, arr[i], i, arr);
-	}
-	return sum;
+  let initial = initialValue;
+  if (initialValue === null || initialValue === undefined) {
+    initial = arr[0];
+  }
+  let sum = initial;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum = func.call(arr, sum, arr[i], i, arr);
+  }
+  return sum;
 }
